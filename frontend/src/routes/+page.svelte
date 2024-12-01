b9a75048d484087dfb7d2744d4f76bf1f10282ff
Sama Ahmed <26samahmed@csu.fullerton.edu>
2024-11-30 21:21:12 -0800

    Make design responsive

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -12,39 +12,42 @@
 
 </script>
 
-<main>
-  <h1 class="text-center text-6xl mt-8 text-[#1C873E] font-medium ">Welcome to Playlist Exporter!</h1>
-  <p class="text-center text-2xl mt-5 text-[#7e7C7C] mb-6">Start Converting Your Playlists from Spotify to YouTube</p>
-  <div class="flex flex-col items-center space-y-4">
-    <a href="/auth/spotify" 
-       class="md:text-2xl text-center text-white bg-green-500 py-2 px-4 m-5 rounded hover:bg-green-600">
-      Login with Spotify
-    </a>
-    <a href="/auth/youtube" 
-       class="md:text-2xl text-center text-white bg-red-500 py-2 px-4 rounded hover:bg-red-600">
-      Login with Youtube
-    </a>
+<main class="flex flex-col justify-between h-screen">
+
+  <div>
+    <h1 class="text-center text-3xl md:text-6xl mt-8 text-[#1C873E] font-medium">
+      Welcome to Playlist Exporter!
+    </h1>
+    <p class="text-center text-xl md:text-2xl mt-5 text-[#7e7C7C] mb-6">
+      Start Converting Your Playlists from Spotify to YouTube
+    </p>
+    <div class="flex flex-col items-center space-y-4">
+      <a href="/auth/spotify" 
+         class="md:text-2xl text-center text-white bg-green-500 py-2 px-4 m-5 rounded hover:bg-green-600">
+        Login with Spotify
+      </a>
+      <a href="/auth/youtube" 
+         class="md:text-2xl text-center text-white bg-red-500 py-2 px-4 rounded hover:bg-red-600">
+        Login with Youtube
+      </a>
+    </div>
   </div>
-  
 
-  <p class="text-center md:text-xl mt-16 mb-5 text-[#FFAAAA]">
-    Here is a song recommendation while you login!
-  </p>
-
-  <iframe 
-  style="border-radius:12px; display:block; margin:auto;" 
-  class="w-full sm:w-96"
-  src="https://open.spotify.com/embed/track/52dF752HWIsv2CpSuTX3p2?utm_source=generator" 
-  height="100"
-  frameBorder="0" 
-  allowfullscreen 
-  allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
-  loading="lazy" 
-  title="Spotify Song Player">
-</iframe>
-
+  <div class="text-center">
+    <p class="md:text-xl text-[#FFAAAA] mb-4">
+      Here is a song recommendation while you login!
+    </p>
+    <iframe 
+      class="w-full sm:w-96 h-24 rounded-lg m-auto" 
+      src="https://open.spotify.com/embed/track/52dF752HWIsv2CpSuTX3p2?utm_source=generator" 
+      frameborder="0" 
+      allowfullscreen 
+      allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
+      loading="lazy" 
+      title="Spotify Song Player">
+    </iframe>
+  </div>
 </main>
-
 
 <style>
   :global(body) {
